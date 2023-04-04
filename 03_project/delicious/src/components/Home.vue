95a404265b216196c763827e231e5af91cdf980b
kemiedon <kemiedon@gmail.com>
2023-04-04 21:11:15 +0800

    2023-04-04  v1.5
    ### 更新
    - 點餐系統剩購物車、結帳流程待完成

--- 03_project/delicious/src/components/Home.vue
+++ 03_project/delicious/src/components/Home.vue
@@ -1,30 +1,81 @@
+<script setup>
+
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+var cart = ref([])
+
+var pastas = ref([])
+//取得pasta資料
+const getPastas = async () => {
+    try{
+        const response = await axios.get('/json/pasta.json')
+        pastas.value = response.data
+        
+    }catch(error){
+        console.log(error)
+    }
+}
+//宣告desserts變數
+var desserts = ref([])
+//取得desserts json資料
+const getDesserts = async() => {
+    try{
+        const response = await axios.get('/json/dessert.json')
+       
+        desserts.value = response.data
+        console.log('get_dessert')
+    }catch(error){
+        console.log(error)
+    }
+}
+var drinks = ref([])
+//取得drink json資料
+const getDrinks = async() => {
+    try{
+        const response = await axios.get('/json/drink.json')
+        drinks.value = response.data
+    }catch(error){
+        console.log(error)
+    }
+}
+function addPastaToCart(index){
+
+}
+onMounted(() => {
+    getPastas()
+    getDesserts()
+    getDrinks()
+})
+
+</script>
 <template>
     <div class="col-md-12">
         <h3>歡迎來到Delicious點餐系統，請選擇您要的餐點</h3>
         <div class="container">
-            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <ul class="nav nav-pills mb-3 d-flex justify-content-center" id="pills-tab" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
-                        data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
+                    <button v-if="pastas.length > 0" class="nav-link active" id="pills-pasta-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-pasta" type="button" role="tab" aria-controls="pills-pasta"
                         aria-selected="true" @click="getPastas()">主餐</button>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
-                        data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile"
+                    <button v-if="desserts.length > 0" class="nav-link" id="pills-dessert-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-dessert" type="button" role="tab" aria-controls="pills-dessert"
                         aria-selected="false" @click="getDesserts()">甜點</button>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill"
-                        data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact"
-                        aria-selected="false">飲料</button>
+                    <button v-if="drinks.length > 0" class="nav-link" id="pills-drink-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-drink" type="button" role="tab" aria-controls="pills-drink"
+                        aria-selected="false" @click="getDrinks()">飲料</button>
                 </li>
             </ul>
             <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                    <div class="row" v-if="pastas.length > 0">
-                        <div class="col-md-3 mt-3" v-for="(pasta, i) in pastas" :key ="i">
+                <div class="tab-pane fade show active" id="pills-pasta" role="tabpanel" aria-labelledby="pills-pasta-tab">
+                    <div class="row" >
+                        <div class="col-md-3 mt-3" v-for="pasta in pastas" :key ="pasta.id">
                             <div class="card">
-                                <img :src="pasta_imgs[i]" class="card-img-top" alt="...">
+                                <img :src="`./src/assets/imgs/menu/${pasta.image}`" class="card-img-top" :alt="pasta.name">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ pasta.name }}</h5>
                                     <p class="card-text">{{ pasta.description }}</p>
@@ -36,30 +87,30 @@
                         
                     </div>
                 </div>
-                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                    <div class="row" v-if="desserts.length > 0">
-                        <div class="col-md-3 mt-3" v-for="(dessert, j) in desserts" :key="j">
+                <div class="tab-pane fade" id="pills-dessert" role="tabpanel" aria-labelledby="pills-dessert-tab">
+                    <div class="row">
+                        <div class="col-md-3 mt-3" v-for="dessert in desserts" :key="dessert.id">
                             <div class="card">
-                                <img :src="dessert_imgs[i]" class="card-img-top" :alt="dessert.name">
+                                <img :src="`./src/assets/imgs/menu/${dessert.image}`" class="card-img-top" :alt="dessert.name">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ dessert.name }}</h5>
                                     <p class="card-text">{{ dessert.description }}</p>
-                                    <p class="price">{{ dessert.price }}</p>
+                                    <p class="price">NT$ {{ dessert.price }}</p>
                                     <button type="button" class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-cart-plus"></i> 加入購物車</button>
                                 </div>
                             </div>
-                        </div>
-                        
+                        </div>       
                     </div>
                 </div>
-                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                <div class="tab-pane fade" id="pills-drink" role="tabpanel" aria-labelledby="pills-drink-tab">
                     <div class="row">
-                        <div class="col-md-3 mt-3">
+                        <div class="col-md-3 mt-3" v-for="drink in drinks" :key="drink.id">
                             <div class="card">
-                                <img src="../assets/imgs/menu/drink1.jpg" class="card-img-top" alt="...">
+                                <img :src="`./src/assets/imgs/menu/${drink.image}`" class="card-img-top" :alt="drink.name">
                                 <div class="card-body">
-                                    <h5 class="card-title">法式燉牛肉</h5>
-                                    <p class="card-text">說明</p>
+                                    <h5 class="card-title">{{drink.name}}</h5>
+                                    <p class="card-text">{{drink.description}}</p>
+                                    <p class="price">NT$ {{drink.price}}</p>
                                     <button type="button" class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-cart-plus"></i> 加入購物車</button>
                                 </div>
                             </div>
@@ -72,44 +123,7 @@
     </div>
 </template>
 
-<script setup>
 
-import axios from 'axios'
-import { reactive, onMounted, onUpdated, watchEffect } from 'vue'
-// 載入pasta.json資料
-var pastas = reactive([])
-var pasta_imgs = reactive([])
-const getPastas = async() => {
-    try{
-        const response = await axios.get('/json/pasta.json')
-        pastas = response.data
-        pastas.forEach((item) => {
-            pasta_imgs.push(`./src/assets/imgs/menu/${item.image}`)
-        })
-    }catch(error){
-        console.log(error)
-    }
-}
-var desserts = reactive([])
-var dessert_imgs = reactive([])
-const getDesserts = async() => {
-    try{
-        const response = await axios.get('/json/dessert.json')
-        desserts = response.data
-        desserts.forEach((item) =>{
-            dessert_imgs.push(`./src/assets/imgs/menu/${item.image}`)
-        })
-        console.log('get_dessert')
-    }catch(error){
-        console.log(error)
-    }
-}
-onMounted(() => {
-    getPastas()
-    getDesserts()
-})
-
-</script>
 
 <style>
     h3 {
@@ -117,6 +131,9 @@ onMounted(() => {
     }
     .card{
         padding: 0;
+    }
+    h5{
+        font-family: 'Noto Sans TC', sans-serif;
     }
     .price{
         font-size: 1.2rem;
--- 03_project/delicious/src/components/Order.vue
+++ 03_project/delicious/src/components/Order.vue
@@ -1,5 +1,165 @@
 <template>
-  <h1>Order</h1>
+  <div class="container py-5">
+    <h1 class="py-5 text-center">我的訂單</h1>
+    <div id="my-order" class="d-flex justify-content-center">
+
+      <div class="accordion col-md-9" id="accordionPanelsStayOpenExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+              aria-controls="panelsStayOpen-collapseOne">
+              訂單 #1 2023-04-02
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
+            aria-labelledby="panelsStayOpen-headingOne">
+            <div class="accordion-body d-flex justify-content-center">
+              <ul class="col-md-8 list-group mb-3 sticky-top">
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">海鮮義大利麵</h6>
+
+                  </div>
+                  <div class="col-md-4 d-flex justify-content-between">
+                    <span class="text-muted">x 6</span>
+                    <span class="text-muted">$12</span>
+                  </div>
+                </li>
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">青醬義大利麵</h6>
+
+                  </div>
+                  <div class="col-md-4 d-flex justify-content-between">
+                    <span class="text-muted">x 6</span>
+                    <span class="text-muted">$12</span>
+                  </div>
+                </li>
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">焦糖布丁</h6>
+
+                  </div>
+                  <div class="col-md-4 d-flex justify-content-between">
+                    <span class="text-muted">x 6</span>
+                    <span class="text-muted">$12</span>
+                  </div>
+                </li>
+
+                <li class="list-group-item d-flex justify-content-between">
+                  <span>總金額 (NTD)</span>
+                  <strong>2000</strong>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseTwo">
+              訂單 #2 2023-03-22
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse"
+            aria-labelledby="panelsStayOpen-headingTwo">
+            <div class="accordion-body d-flex justify-content-center">
+              <ul class="col-md-8 list-group mb-3 sticky-top">
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">海鮮義大利麵</h6>
+
+                  </div>
+                  <div class="col-md-4 d-flex justify-content-between">
+                    <span class="text-muted">x 6</span>
+                    <span class="text-muted">$12</span>
+                  </div>
+                </li>
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">青醬義大利麵</h6>
+
+                  </div>
+                  <div class="col-md-4 d-flex justify-content-between">
+                    <span class="text-muted">x 6</span>
+                    <span class="text-muted">$12</span>
+                  </div>
+                </li>
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">焦糖布丁</h6>
+
+                  </div>
+                  <div class="col-md-4 d-flex justify-content-between">
+                    <span class="text-muted">x 6</span>
+                    <span class="text-muted">$12</span>
+                  </div>
+                </li>
+
+                <li class="list-group-item d-flex justify-content-between">
+                  <span>總金額 (NTD)</span>
+                  <strong>2000</strong>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseThree">
+              訂單 #3 2023-01-02
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse"
+            aria-labelledby="panelsStayOpen-headingThree">
+            <div class="accordion-body d-flex justify-content-center">
+              <ul class="col-md-8 list-group mb-3 sticky-top">
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">海鮮義大利麵</h6>
+
+                  </div>
+                  <div class="col-md-4 d-flex justify-content-between">
+                    <span class="text-muted">x 6</span>
+                    <span class="text-muted">$12</span>
+                  </div>
+                </li>
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">青醬義大利麵</h6>
+
+                  </div>
+                  <div class="col-md-4 d-flex justify-content-between">
+                    <span class="text-muted">x 6</span>
+                    <span class="text-muted">$12</span>
+                  </div>
+                </li>
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">焦糖布丁</h6>
+
+                  </div>
+                  <div class="col-md-4 d-flex justify-content-between">
+                    <span class="text-muted">x 6</span>
+                    <span class="text-muted">$12</span>
+                  </div>
+                </li>
+
+                <li class="list-group-item d-flex justify-content-between">
+                  <span>總金額 (NTD)</span>
+                  <strong>2000</strong>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
