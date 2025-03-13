37b2e354ad708f3872895ca3c8dd6adcfd108599
kemiedon <kemiedon@gmail.com>
2025-03-13 21:33:09 +0800

    Update Child.vue

--- 02_useVue/data_send/src/components/Child.vue
+++ 02_useVue/data_send/src/components/Child.vue
@@ -1,37 +1,13 @@
+<!-- Child.vue -->
 <script setup>
-import { ref } from 'vue'
-
-const props = defineProps({
-  msg: String,
-})
-let inputValue = ref(props.msg);
-
-const emit = defineEmits(['update:message'])
-
-const sendMessage = () =>{
-  emit('update:message', inputValue);
-}
+defineProps(['name', 'age'])
 </script>
 
 <template>
-  <h1>子組件</h1>
-  <div class="card">
-    <div>
-      <input type="text" v-model="inputValue" placeholder="Enter message">
-    </div>
-    
-    <button type="button" @click="sendMessage">傳送訊息至父組件</button>
-  </div>
-
+  <p>名字：{{ name }}</p>
+  <p>年齡：{{ age }}</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
-input{
-  margin-bottom: 20px;
-  height: 40px;
-  border-radius: 5px;
-}
+
 </style>
